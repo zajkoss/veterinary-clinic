509556f03d45e917e156f48918f17e820f507112
Lukasz <lukasz100.96@gmail.com>
2022-04-12 23:06:24 +0200

    Add rest controller to manage doctor

--- src/main/java/pl/kurs/veterinaryclinic/controller/DoctorController.java
+++ src/main/java/pl/kurs/veterinaryclinic/controller/DoctorController.java
@@ -1,12 +1,25 @@
 package pl.kurs.veterinaryclinic.controller;
 
 import org.modelmapper.ModelMapper;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.*;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import pl.kurs.veterinaryclinic.commands.CreateDoctorCommand;
+import pl.kurs.veterinaryclinic.dto.DoctorDto;
+import pl.kurs.veterinaryclinic.dto.StatusDto;
+import pl.kurs.veterinaryclinic.model.Doctor;
 import pl.kurs.veterinaryclinic.service.DoctorService;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(path = "/doctors")
+@Validated
 public class DoctorController {
 
     private DoctorService doctorService;
@@ -16,5 +29,40 @@ public class DoctorController {
         this.doctorService = doctorService;
         this.mapper = mapper;
     }
+
+    @GetMapping()
+    public ResponseEntity<List<DoctorDto>> getAll(
+            @PageableDefault Pageable pageable
+    ) {
+        Page<Doctor> loadedDoctorPage = doctorService.getAll(pageable);
+        return ResponseEntity.ok(
+                loadedDoctorPage
+                        .getContent()
+                        .stream()
+                        .map(doctor -> mapper.map(doctor, DoctorDto.class))
+                        .collect(Collectors.toList())
+
+        );
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DoctorDto> getDoctorById(@PathVariable("id") long id) {
+        return ResponseEntity.ok(
+                mapper.map(doctorService.get(id), DoctorDto.class)
+        );
+    }
+
+    @PostMapping
+    public ResponseEntity<DoctorDto> addDoctor(@RequestBody @Valid CreateDoctorCommand doctor) {
+        Doctor newDoctor = mapper.map(doctor, Doctor.class);
+        return ResponseEntity.status(HttpStatus.CREATED).body(mapper.map(doctorService.add(newDoctor), DoctorDto.class));
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<StatusDto> softDeleteDoctor(@PathVariable("id") long id) {
+        doctorService.softDelete(id);
+        return ResponseEntity.ok().body(new StatusDto("" + id));
+    }
+
 
 }
