3b7b425854aa3b90da5c26a83adc5e6989add6cc
Lukasz <lukasz100.96@gmail.com>
2022-04-12 18:49:02 +0200

    Initial commit

---
+++ src/test/java/pl/kurs/veterinaryclinic/VeterinaryClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kurs.veterinaryclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeterinaryClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
