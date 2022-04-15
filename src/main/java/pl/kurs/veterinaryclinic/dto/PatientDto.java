5555963bd08dc2fe956c220ecaf822c424e0c128
Lukasz <lukasz100.96@gmail.com>
2022-04-16 00:14:05 +0200

    Add main dto for patient

---
+++ src/main/java/pl/kurs/veterinaryclinic/dto/PatientDto.java
@@ -0,0 +1,84 @@
+package pl.kurs.veterinaryclinic.dto;
+
+public class PatientDto {
+
+    private Long id;
+
+    private String name;
+
+    private String species;
+
+    private String breed;
+
+    private Integer age;
+
+    private String ownerName;
+
+    private String ownerSurname;
+
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+    public String getOwnerSurname() {
+        return ownerSurname;
+    }
+
+    public void setOwnerSurname(String ownerSurname) {
+        this.ownerSurname = ownerSurname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
