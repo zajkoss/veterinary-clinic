048fc2e78dda9f2c524d43707d11ffab2c01319f
Lukasz <lukasz100.96@gmail.com>
2022-04-23 12:12:44 +0200

    Add format to datetime

--- src/main/java/pl/kurs/veterinaryclinic/dto/AvailableVisitDto.java
+++ src/main/java/pl/kurs/veterinaryclinic/dto/AvailableVisitDto.java
@@ -1,5 +1,7 @@
 package pl.kurs.veterinaryclinic.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.time.LocalDateTime;
 import java.util.Objects;
 
@@ -7,6 +9,7 @@ public class AvailableVisitDto {
 
     private DoctorNameDto doctor;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime date;
 
     public AvailableVisitDto(DoctorNameDto doctor, LocalDateTime date) {
