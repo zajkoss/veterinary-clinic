b2f6380e9a2fd2948c76a6714ee37f5155744bdd
Lukasz <lukasz100.96@gmail.com>
2022-04-27 20:40:42 +0200

    Enable async task for app

--- src/main/java/pl/kurs/veterinaryclinic/VeterinaryClinicApplication.java
+++ src/main/java/pl/kurs/veterinaryclinic/VeterinaryClinicApplication.java
@@ -2,11 +2,13 @@ package pl.kurs.veterinaryclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
 
 @SpringBootApplication
+@EnableAsync
 public class VeterinaryClinicApplication {
 
     @PostConstruct
